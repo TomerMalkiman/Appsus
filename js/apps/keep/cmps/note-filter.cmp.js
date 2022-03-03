abfa37c650b7dfff489d480759673ed0c46afbb7
BenIsayev <benisayev@gmail.com>
2022-03-03 19:34:36 +0200

    some design and note filter option

--- js/apps/keep/cmps/note-filter.cmp.js
+++ js/apps/keep/cmps/note-filter.cmp.js
@@ -1,16 +1,33 @@
 export default {
     // props: [""],
     template: `
-        <section>
-            AA
+        <section class="note-filter">
+            <nav>
+                <div @click="setFilter('all')" class="text"><img src="../../../../img/keep-icons/all.png"/> All</div>
+                <div @click="setFilter('note-txt')" class="text"><img src="../../../../img/keep-icons/text.png"/> Text</div>
+                <div @click="setFilter('note-video')" class="video"><img src="../../../../img/keep-icons/video.png"/>Video</div>
+                <div @click="setFilter('note-img')" class="img"><img src="../../../../img/keep-icons/image.png"/>Image</div>
+                <div @click="setFilter('note-todos')" class="todos"><img src="../../../../img/keep-icons/list.png"/>Todo Lists</div>
+                <div @input="setFilter(filterBy.type)" class="text-search"><input type="text" placeholder="Search By Text.." v-model="filterBy.txt"></div>
+            </nav>
         </section>
     `,
     components: {},
     created() {},
     data() {
-        return {}
+        return {
+            filterBy: {
+                type: 'note-txt',
+                txt: '',
+            }
+        }
     },
-    methods: {},
+    methods: {
+        setFilter(type) {
+            this.filterBy.type = type
+            this.$emit('filter-set', this.filterBy)
+        }
+    },
     computed: {},
     unmounted() {},
 }
--- js/apps/keep/cmps/note-list.cmp.js
+++ js/apps/keep/cmps/note-list.cmp.js
@@ -6,11 +6,11 @@ export default {
         <section class="notes main-layout">
             <section class="note-list" v-if="pinnedNotes">
                 <h4 class="pinned-headline">Pinned</h4>
-                <note-preview v-for="note in pinnedNotes" :key="note.id" :note="note" @note-pinned="togglePin" @note-deleted="deleteNote" @note-bgc-updated="updateBgc" @todo-done="toggleTodo"/>
+                <note-preview v-for="note in pinnedNotes" :key="note.id" :note="note" @note-pinned="togglePin" @note-deleted="deleteNote" @note-bgc-updated="updateBgc" @todo-done="toggleTodo" @note-duplicate="duplicateNote"/>
             </section>
             <section class="note-list" v-if="unPinnedNotes">
                 <h4 class="pinned-headline">Other</h4>
-                <note-preview v-for="note in unPinnedNotes" :key="note.id" :note="note" @note-pinned="togglePin" @note-deleted="deleteNote" @note-bgc-updated="updateBgc" @todo-done="toggleTodo"/>
+                <note-preview v-for="note in unPinnedNotes" :key="note.id" :note="note" @note-pinned="togglePin" @note-deleted="deleteNote" @note-bgc-updated="updateBgc" @todo-done="toggleTodo" @note-duplicate="duplicateNote"/>
             </section>
         </section>
     `,
@@ -33,6 +33,9 @@ export default {
         },
         toggleTodo(todoId, noteId) {
             this.$emit('todo-done', todoId, noteId)
+        },
+        duplicateNote(noteId) {
+            this.$emit('note-duplicate', noteId)
         }
     },
     computed: {},
